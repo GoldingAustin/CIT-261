e82b037d7c660f9ab913853f974f4b94cf287a2c
GoldingAustin <gol13014@byui.edu>
2016-11-15 23:33:48 -0700

    sepetated css, finalized login

--- Final/login.js
+++ Final/login.js
@@ -1,6 +1,19 @@
         window.onload = function() {
             document.getElementById("login").onclick = function() {
                 var input = document.getElementById("input").value;
-                localStorage.setItem("username", input);
+                var text = /(^\S*([A-Za-z0-9])*\S*)$/;
+                if (input.match(text) && input !== "") {
+                    localStorage.setItem("username", input);
+                    location.assign("final.html");
+                } else {
+                    document.getElementById("error").style.display = "block";
+                }
             };
+        };
+
+        document.getElementById("input").onfocus = function() {
+            this.style.boxShadow = "0px 0px 15px red";
+        };
+        document.getElementById("input").onblur = function() {
+            this.style.boxShadow = "";
         };
